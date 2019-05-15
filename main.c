42cf4167ed2a96ecfa24de737376262c4b33dff6
Maisha <misha-00per@mail.ru>
2019-05-15 12:33:53 +0700

    prototipi

--- main.c
+++ main.c
@@ -4,3 +4,8 @@
 #include <string.h>
 #include <iostream>
 using namespace std;
+void Fillfg(char A[], char h[], int& i);
+void Fillfgs(char A[], char h[]);
+void Fillxch(char A[], char h[], int& i);
+void Fillych(char A[], char h[], int& i);
+void Fillrch(char A[], char h[], int& i);
